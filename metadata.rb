409bbbd7786aa808441bac0ec42b24f8e2bd35c8
Jeroen Jacobs <git@jeroenj.be>
2014-11-28 22:59:34 +0100

    Bumps version to v1.0.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Jeroen Jacobs'
 maintainer_email  'chef@jeroenj.be'
 license           'MIT'
 description       'Installs CouchPotato.'
-version           '1.0.1'
+version           '1.0.2'
 
 depends           'git'
 depends           'python'
