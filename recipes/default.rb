3c0e3fed4da9a8bdd78b6a40f6f8e84bd18ff825
Elijah Wright <elijah.wright@gmail.com>
2016-03-01 10:59:00 -0600

    Add url_base support to couchpotato settings.

--- recipes/default.rb
+++ recipes/default.rb
@@ -61,6 +61,7 @@ template ::File.join(node[:couchpotato][:config_dir], 'settings.conf') do
     twitter_access_token_key: node[:couchpotato][:settings][:twitter_access_token_key],
     twitter_access_token_secret: node[:couchpotato][:settings][:twitter_access_token_secret],
     twitter_username: node[:couchpotato][:settings][:twitter_username],
+    urlbase: node[:couchpotato][:settings][:urlbase],
     username: node[:couchpotato][:settings][:username]
   )
   notifies :restart, 'service[couchpotato]'
